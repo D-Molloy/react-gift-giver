2d19c85b641a6ea95185108be1415de4afcafa80
Denis Molloy <denis.molloy@gmail.com>
2018-05-08 18:57:07 -0700

    added BDD tests in app.test.js

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -18,13 +18,20 @@ class App extends Component{
         const max_id = ids.length > 0 ? Math.max(...ids) : 0;
         
         gifts.push({ id: max_id + 1})
-
+        //{gifts} === {gifts:gifts}
         this.setState({gifts})
     }
     render(){
         return (
             <div>
                 <h2>Gift Giver</h2>
+                <div className="gift-list">
+                {this.state.gifts.map(gift => {
+                    return(
+                        <div key={gift.id}></div>
+                    )
+                })}
+                </div>
                 <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
             </div>
         )
--- giftgiver/src/components/app.test.js
+++ giftgiver/src/components/app.test.js
@@ -1,26 +1,63 @@
 //fundamental test for a component - testing if a component renders on the DOM
 import React from 'react';
 // shallow - helper function from Enzyme - allow us to shallowly render a parent component
-import {shallow} from 'enzyme';
+import {
+	shallow
+} from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />)
 
-//unit test for rendering the app component
-it("renders correctly", () =>{
-    expect(app).toMatchSnapshot()
-})
 
-//make sure the app initializes the state with a empty list of gifts
-it("initializes the `state` with an empty list of gifts", () => {
-    expect(app.state().gifts).toEqual([]);
-})
+//grouping tests together for just the App component
+describe("App", () => {
+	const app = shallow( < App / > )
 
-it("add a new gift to `state` when clicking on the `add gift` button", ()=>{
-    //need to find and click on the `add gift` button to run the test
-    //find inner child nodes or components by their JSX or className
-    app.find(".btn-add").simulate('click');
-    //expect that clicking on the button with an empty state adds a gift with an id = 1
+	{ /* unit test for rendering the app component */ }
+	it("renders correctly", () => {
+		expect(app).toMatchSnapshot()
+	})
 
-    expect(app.state().gifts).toEqual([{id:1}]);
+	{ /* //make sure the app initializes the state with a empty list of gifts */ }
+
+	it("initializes the `state` with an empty list of gifts", () => {
+		expect(app.state().gifts).toEqual([]);
+	})
+
+	{
+		/* // having to tests where we are clicking on the same button results in receiving errors for both
+		    //this is test pollution */
+	}
+
+	describe("when clicking the `add gift` button", () => {
+
+		// beforeEach - runs some code before each test in the describe block
+		// since we were doing a button click in both it statements below, we put it in the before each
+		beforeEach(()=>{
+			app.find(".btn-add").simulate('click');
+		})
+
+		//after each test we rest the state back to empty gifts array
+		afterEach(()=>{
+			app.setState({gifts: []})
+		})
+
+		it("add a new gift to `state`", () => {
+			//need to find and click on the `add gift` button to run the test
+			//find inner child nodes or components by their JSX or className
+			// app.find(".btn-add").simulate('click');
+			//expect that clicking on the button with an empty state adds a gift with an id = 1
+
+			expect(app.state().gifts).toEqual([{
+				id: 1
+			}]);
+		})
+
+		{ /* //when we click on the `add gift` button we want the GiftList Component to display a new Gift Component inside it */ }
+		it("adds a new gift to the rendered list", () => {
+			// app.find(".btn-add").simulate('click');
+
+			expect(app.find(".gift-list").children().length).toEqual(1);
+		})
+	})
+
 })
