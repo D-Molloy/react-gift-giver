f208f17b41577de9ef61edc6e36472b162feb247
Denis Molloy <denis.molloy@gmail.com>
2018-05-12 00:41:20 -0700

    created and tested setBalance action

--- lootcheck/src/actions/balance.js
+++ lootcheck/src/actions/balance.js
@@ -3,6 +3,8 @@ import * as constants from './constants';
 // an action is an object, we're testing the action creators that make the actions
 // action creator is a function that returns an action with a specific type, but gives it dynamic data based on what we pass as arguments to the action creator function
 
+
+//here we successfully create an object that represents the redux action
 export const setBalance = balance =>{
     return {
         type: constants.SET_BALANCE,
