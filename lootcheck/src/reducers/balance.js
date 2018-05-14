bf13b3a46a433223519f9178e1478fb0c45eb972
Denis Molloy <denis.molloy@gmail.com>
2018-05-13 22:13:23 -0700

    BALANCE REDUCER - implemented cookies

--- lootcheck/src/reducers/balance.js
+++ lootcheck/src/reducers/balance.js
@@ -1,21 +1,45 @@
 import * as constants from '../actions/constants';
+//saving the balance in cookies
+// read_cookie takes a string for it argument (the cookie we want to read)
 
+///bake_cookie take a string for the first argument(to identify the cookie we want to bake) and the second parameter is the data we want to store
+import {
+    read_cookie,
+    bake_cookie
+} from 'sfcookies';
+
+//the string for the arguments (cookies)
+const BALANCE_COOKIE = "BALANCE_COOKIE"
+
+// which takes the place of returning the default state
+// default:
+// return state;
 
 //we have the balance action, but now we need a balance reducer
 // a reducer is the way that redux knows how to update the store based on certain actions
 //reducers (like actionCreators) are pure function with unique results solely based on the input
 //state should always have a default value
 const balance = (state = 0, action) => {
+    let balance;
+
     switch (action.type) {
         case constants.SET_BALANCE:
-            return action.balance;
+            balance = action.balance;
+            break;
         case constants.DEPOSIT:
-            return state + action.deposit;
+            balance = state + action.deposit;
+            break;
         case constants.WITHDRAW:
-            return state - action.withdrawal;
+            balance = state - action.withdrawal;
+            break;
         default:
-            return state;
+            balance = parseInt(read_cookie(BALANCE_COOKIE), 10) || state;
     }
+
+    //jest simulates a mock window object complete with cookies
+    bake_cookie(BALANCE_COOKIE, balance);
+
+    return balance;
 }
 
 export default balance;
--- lootcheck/src/reducers/balance.test.js
+++ lootcheck/src/reducers/balance.test.js
@@ -2,14 +2,27 @@
 // a reducer is the way that redux knows how to update the store based on certain actions
 //reducers (like actionCreators) are pure function with unique results solely based on the input
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
-describe("balanceReducer", ()=>{
+describe("balanceReducer", () => {
+    const balance = 10;
+    describe("when initializing", () => {
+        
+        it("sets a balance", () => {
 
-    it("sets a balance", ()=>{
-        const balance = 10;
 
-        expect(balanceReducer(undefined, {type: constants.SET_BALANCE, balance})).toEqual(balance);
+            expect(balanceReducer(undefined, {
+                type: constants.SET_BALANCE,
+                balance
+            })).toEqual(balance);
+        })
+    })
+
+    describe("the re-initializing", () => {
+        it("reads the balance from cookies", () => {
+            expect(balanceReducer2(undefined, {})).toEqual(balance)
+        })
     })
 
     //test for depositing into the balance
@@ -17,18 +30,21 @@ describe("balanceReducer", ()=>{
         const deposit = 10;
         const initialState = 5;
         //previous state is the first parameter of the reducer, followed by the action
-        expect(balanceReducer(initialState, {type: constants.DEPOSIT, deposit})).toEqual(initialState + deposit);
-    }) 
+        expect(balanceReducer(initialState, {
+            type: constants.DEPOSIT,
+            deposit
+        })).toEqual(initialState + deposit);
+    })
 
     //testing the withdrawal portion of the reducer
     it('withdraws from the balance', () => {
         const withdrawal = 10;
         const initialState = 20;
         //previous state is the first parameter of the reducer, followed by the action
-        expect(balanceReducer(initialState, {type: constants.WITHDRAW, withdrawal})).toEqual(initialState - withdrawal);
-    }) 
+        expect(balanceReducer(initialState, {
+            type: constants.WITHDRAW,
+            withdrawal
+        })).toEqual(initialState - withdrawal);
+    })
 
 });
-
-
-
