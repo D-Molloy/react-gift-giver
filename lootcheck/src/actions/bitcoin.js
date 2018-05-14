6396e9741c9bd34e3dd8a6eb084afd3f0d8f9874
Denis Molloy <denis.molloy@gmail.com>
2018-05-14 13:23:28 -0700

    implemented tests for bitcoin and bitcoin action

---
+++ lootcheck/src/actions/bitcoin.js
@@ -0,0 +1,15 @@
+import {FETCH_BITCOIN} from './constants';
+
+export const fetchBitcoin = () => {
+    //the way the redux-thunk middleware works is  that when we want to return an async action (like a promise), the thunk middleware takes it and gives us a dispatch function that represents the dispatch ability of the redux store
+    //this dispatch is extra powerful - we can return the async fetch
+    //the dispatch wraps around the async function we want to return
+    return dispatch => {
+        //returning the promise from the async code
+        return fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+        //handle the response
+        .then(response => response.json())
+        //boil down the json further - final dispatch to the redux store
+        .then(json => dispatch({type: FETCH_BITCOIN, bitcoin: json }))
+    }
+}
---
+++ lootcheck/src/actions/bitcoin.test.js
@@ -0,0 +1,38 @@
+// https://api.coindesk.com/v1/bpi/currentprice.json
+
+///need to configure a mock store in order to test the async actions
+import configureMockStore from 'redux-mock-store';
+// need to used middleware that will allow us to return actions that aren't objects (like a function or promise)
+import thunk from 'redux-thunk';
+//using a mock version of the native JS fetch method
+import fetchMock from 'fetch-mock';
+
+//constant for our action
+import { FETCH_BITCOIN } from './constants';
+//importing the action
+import { fetchBitcoin } from './bitcoin';
+
+//configureMockStore allows us to apply middleware
+const createMockStore = configureMockStore([thunk]);
+// creating the mockStore
+const store = createMockStore({ bitcoin: {} });
+
+const mockResponse = { body: { bpi: 'bitcoin price index'}};
+
+//fetch-mock's get method
+// params:
+// 1 - string of the endpoint we want to stub
+// 2 - object of what we want the stubbed endpoint to return
+fetchMock.get("https://api.coindesk.com/v1/bpi/currentprice.json", mockResponse);
+
+describe("it creates an async action to fetch the bitcoin value", ()=>{
+    //the async fetchMock function will return a promise that redux can handle
+    //getActions shows what actions are actually dispatched to the store
+    const expectedActions = [{bitcoin: mockResponse.body, type: FETCH_BITCOIN}];
+    //calling the action
+    //in order for jest to process the promise, we have to return the promise
+    return store.dispatch(fetchBitcoin()).then(()=> {
+        expect(store.getActions()).toEqual(expectedActions);
+    })
+    
+})
